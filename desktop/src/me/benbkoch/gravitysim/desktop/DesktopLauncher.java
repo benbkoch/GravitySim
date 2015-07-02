d19b4f0385dd160c71ab90e3e4afb946d4782d1f
benbkoch <benbkoch@gmail.com>
2015-07-01 19:27:34 -0700

    Update DesktopLauncher.java

--- desktop/src/me/benbkoch/gravitysim/desktop/DesktopLauncher.java
+++ desktop/src/me/benbkoch/gravitysim/desktop/DesktopLauncher.java
@@ -7,7 +7,7 @@ import me.benbkoch.gravitysim.GravitySim;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.title = "Gravity Simulator by Travis Thompson and Ben Koch";
+		config.title = "Gravity Simulator by Ben Koch";
 		config.width = 1920;
 		config.height = 1080;
 		new LwjglApplication(new GravitySim(), config);
